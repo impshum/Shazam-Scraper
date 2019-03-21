a4ef925fe720d5aa480737131ede02c3a14ac200
impshum <impshum@sausage.local>
2019-03-21 18:33:18 +0000

    up

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# shazam scraper
+# Shazam Scraper
+
+### About the scripts
+
+#### top.py
+
+- Scrapes Shazam for top 100 track (or given amount) for each country available
+- Creates csv files with the data (e.g. tracks-21-03-2019.csv)
+
+#### tracks.py
+
+- Scrapes Shazam for the position in the charts for a given list of tracks
+- Creates csv files with the data (e.g. argentina-21-03-2019.csv)
+
+### Requirements
+
+- Python 3
+- Splinter (Selenium)
+- BeautifulSoup
+
+### Instructions
+
+- Fill in track numbers in config
+- Choose limit in config
+- Choose if headless or not in config
+- Install requirements
+- Run it
--- config.py
+++ config.py
@@ -1,15 +1,13 @@
-tracks = ['455443132']
+countries = ['argentina', 'australia', 'austria', 'belarus', 'belgium', 'brazil', 'bulgaria', 'canada',
+             'chile', 'china', 'columbia', 'costa rica', 'croatia', 'czeck-republic', 'demark', 'egypt',
+             'finland', 'france', 'germany', 'greece', 'hungary', 'india', 'indonesia', 'iran', 'iceland', 'israel',
+             'italy', 'japan', 'kazakhstan', 'Malaysia', 'Mexico', 'Morooco', 'netherlands', 'new-zealand', 'norway',
+             'peru', 'portugal', 'romania', 'russia', 'saudi-arabia', 'singapore', 'south-africa', 'south-korea',
+             'spain', 'sweden', 'switzerland', 'taiwan', 'thailand', 'turkey', 'ukraine', 'united-kingdom',
+             'united-states', 'uruguay', 'venezuela']
 
-#countries = ['argentina', 'australia', 'austria', 'belarus', 'belgium', 'brazil', 'bulgaria', 'canada',
-#             'chile', 'china', 'columbia', 'costa rica', 'croatia', 'czeck-republic', 'demark', 'egypt',
-#             'finland', 'france', 'germany', 'greece', 'hungary', 'india', 'indonesia', 'iran', 'iceland israel',
-#             'italy', 'japan', 'kazakhstan', 'Malaysia', 'Mexico', 'Morooco', 'netherlands', 'new-zealand', 'norway',
-#             'peru', 'portugal', 'romania', 'russia', 'saudi-arabia', 'singapore', 'south-africa', 'south-korea',
-#             'spain', 'sweden', 'switzerland', 'taiwan', 'thailand', 'turkey', 'ukraine', 'united-kingdom',
-#             'united-states', 'uruguay', 'venezuela']
+tracks = ['455443132', '442191576']
 
-countries = ['argentina']
-
-limit = 1
+limit = 100
 
 headless = True
